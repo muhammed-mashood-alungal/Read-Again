11cf238220ab6b1d825b5a54cc25f657bf4c94f1
Muhammed Mashood A <muhdmashoodalungal@gmail.com>
2024-11-06 10:16:29 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const dotenv=require('dotenv')
+
+dotenv.config()
+
+app.listen(process.env.PORT,()=>{
+    console.log("Server Started Succefully")
+})
